baa2a6b19e67f8fde6089ffb882cbd3df99e8d3c
MosNes <7hekarl@gmail.com>
2022-07-10 11:57:27 -0400

    set up server.js with initial config

---
+++ server.js
@@ -0,0 +1,34 @@
+//-----DEPENDENCIES AND GLOBAL VARIABLES-----------------------------------------
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+//creates instance of an express server
+//invoked by `npm start` in terminal
+// use Ctrl+C to end the server
+const app = express();
+
+//-----REQUIRED MIDDLEWARE-------------------------------------------------------
+
+//both of these are required to receive and interpret POST requests
+//parse incoming string or array
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
+
+//required to serve static assets like front-end js and CSS
+//makes the whole 'public' folder available
+//otherwise would need to create endpoints for each js and css file for each page
+app.use(express.static('public'));
+
+
+//------FUNCTIONS----------------------------------------------------------------
+
+//starts listening for requests on port 3001
+//if called on a local PC, starts the server on http://localhost:PORT/
+app.listen(PORT, () => {
+    console.log(`API Server now listening on port ${PORT}!`);
+});
