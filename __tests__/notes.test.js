95e6452920995045b76f73beede967e0348b7ca7
MosNes <7hekarl@gmail.com>
2022-07-10 18:21:30 -0400

    added and tested deleteNote function

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -12,4 +12,27 @@ test('creates a new note object', () => {
 
     expect(note.title).toEqual("Foo");
     expect(note.text).toEqual("Bar");
+    expect(note.id).toEqual(expect.any(String));
+});
+
+test('Deletes a note object', () => {
+    let notesArray = [
+        {
+            title:"Test Title",
+            text:"Test text",
+            id: "bleeplefreeb"
+        },
+        {
+            title:"Test Title 2",
+            text: "Test Text",
+            id: "blorpleflorb"
+        }
+    ];
+
+    notesArray = deleteNote('bleeplefreeb', notesArray);
+    const result2 = deleteNote('blibblefrib', notesArray);
+
+    expect(notesArray.length).toEqual(1);
+    expect(notesArray[0].id).toEqual("blorpleflorb");
+    expect(result2.length).toEqual(1);
 });
