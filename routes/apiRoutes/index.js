e3106cbd2b3be465ddd25a79fa995a485d5173d2
MosNes <7hekarl@gmail.com>
2022-07-10 18:32:29 -0400

    added POST route to index.js in apiRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,6 +2,7 @@
 
 //-----DEPENDENCIES AND GLOBAL VARIABLES-----------------------------------------
 const path = require('path');
+const { createNewNote, deleteNote, validateNote } = require('../../lib/notes');
 const notes = require('../../db/db.json');
 const router = require('express').Router();
 
@@ -10,10 +11,25 @@ const router = require('express').Router();
 //------------ROUTES-------------------------------------
 
 //creates Route to GET notes from db.json
-router.get('/notes',(req,res)=>{
+router.get('/notes', (req, res) => {
     let results = notes;
 
     res.json(results);
 });
+
+//create route to POST new note to db.json
+router.post('/notes', (req, res) => {
+
+    if(!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.')
+    } else {
+        const note = createNewNote(req.body, notes);
+
+        res.json(note);
+    }
+});
+
+//create route to DELETE note from db.json
+
 
 module.exports = router;
